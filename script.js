95249e6bad1d71a57126384a22ce946e50a861c2
nalberga <nicolas.alberga@gmail.com>
2019-11-16 13:37:34 -0500

    watermood update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var appId = 'bcb42cafda271a2eb84b45ee3a3200b8';
 var units = 'imperial';
 
-
 function getsearchMethodValidation(searchTerm) {
   if (Number.isInteger(parseInt(searchTerm)) && searchTerm.length === 5) {
       return 'zip';
@@ -30,25 +29,31 @@ function init(data) {
    document.body.style.backgroundImage = 'url("clear.jpeg")';
    var showMusicPlayer= document.getElementById('music-player-clear');
    showMusicPlayer.style.display = 'block';
+   selectPic = 'sun';
   } else if(data.weather[0].main =="Clouds" || data.weather[0].main =="smoke" || data.weather[0].main =="haze" || data.weather[0].main =="dust" || data.weather[0].main =="fog" || data.weather[0].main =="sand" || data.weather[0].main =="ash" || data.weather[0].main =="squall") {
    document.body.style.backgroundImage = 'url("cloudy.jpeg")';
    var showMusicPlayer= document.getElementById('music-player-inclement');
    showMusicPlayer.style.display = 'block';
+   selectPic = 'clouds';
   } else if(data.weather[0].main =="Rain" || data.weather[0].main =="Drizzle" || data.weather[0].main =="Mist") {
    document.body.style.backgroundImage = 'url("rain.jpeg")';
    var showMusicPlayer= document.getElementById('music-player-rain');
    showMusicPlayer.style.display = 'block';
+   selectPic = 'rain';
   } else if(data.weather[0].main =="Snow") {
    document.body.style.backgroundImage = 'url("snow.jpeg")';
    var showMusicPlayer= document.getElementById('music-player-snow');
    showMusicPlayer.style.display = 'block';
+   selectPic = 'snow';
   } else if(data.weather[0].main =="thunderstorm" || data.weather[0].main =="tornado") {
    document.body.style.backgroundImage = 'url("storm.jpeg")';
    var showMusicPlayer= document.getElementById('music-player-storm');
    showMusicPlayer.style.display = 'block';
+   selectPic = 'storm';
   } else {
    Document.body.style.backgroundImage = 'url("default.jpeg")';
   }
+
   var getcityName = document.getElementById('cityName');
   getcityName.innerHTML = data.name;
   var getTemperature = document.getElementById('temperature');
@@ -74,39 +79,37 @@ function init(data) {
   });
 
   var apiKey = 'LpGRYoMy3u0jMWSHsqfe8CUWKQ9HtnqT';
-  var randomId = 'e826c9fc5c929e0d6c6d423841a282aa';
-  var selectPic = data.weather[0].main;
+  var selectPic ='';
 
-  function searchPictures(selectPic)  {
-   fetch(`http://api.giphy.com/v1/gifs/search?q=${selectPic}&api_key=${apiKey}&limit=10&random_id=${randomId}`)
-  .then(response => { 
-      return response.json();
-    
-    }).then(response => {
-      console.log(response);
-      console.log(response.data);
-      console.log('Meta', response.meta);
-      display(response);
-    })  
+  async function searchPictures(selectPic)  {
+
+    const result = await fetch(`http://api.giphy.com/v1/gifs/search?q=${selectPic}&api_key=${apiKey}&limit=25&`)
+                                .then(res => res.json())
+
+    if (result) {
+      display(result);
+    } else {
+      alert("No Pics Found");
+    }
+  
   }
 
-  function display(result) {
+function display(result) {
+    var img = document.createElement("img");
 
-   // result.forEach(function(choosenPic) {
-    //  console.log(choosenPic);
+    var randomIndex = Math.floor(Math.random() * result.data.length)
 
-  //  })
+    img.src = result.data[randomIndex].images.downsized.url;
 
- var img = document.createElement("img");
- img.src = result.data[2].images.downsized.url;
-  var output = document.getElementsByClassName("output")[0];
-  output.appendChild(img);
+    img.style.height = '250px';
+    img.style.width = '250px';
 
+    var output = document.getElementsByClassName("output")[0];
 
+    if (output.hasChildNodes()) {
+      output.removeChild(output.childNodes[0])
+    }
+
+    output.appendChild(img);
 };
  
-
-
-
- 
-
